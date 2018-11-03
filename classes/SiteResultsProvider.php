ce6658cd86ce95b5b31b76ac60e4bfbd6db32f9a
Brett Hartman <brett.hartman@mac.com>
2018-11-02 21:23:46 -0700

    Complete pagination system

--- classes/SiteResultsProvider.php
+++ classes/SiteResultsProvider.php
@@ -19,10 +19,13 @@
 		}
 
 		public function getResultsHtml($page, $pageSize, $term) {
-			$query = $this->con->prepare("SELECT * FROM sites WHERE title LIKE :term OR url LIKE :term OR keywords LIKE :term OR description LIKE :term ORDER BY clicks DESC");
+			$fromLimit = ($page - 1) * $pageSize;
+			$query = $this->con->prepare("SELECT * FROM sites WHERE title LIKE :term OR url LIKE :term OR keywords LIKE :term OR description LIKE :term ORDER BY clicks DESC LIMIT :fromLimit, :pageSize");
 			$searchTerm = '%' . $term . '%';
 
 			$query->bindParam(":term", $searchTerm);
+			$query->bindParam(":fromLimit", $fromLimit, PDO::PARAM_INT);
+			$query->bindParam(":pageSize", $pageSize, PDO::PARAM_INT);
 			$query->execute();
 
 			$resultsHtml = "<div class='siteResults'>";
@@ -33,12 +36,21 @@
 				$title = $row["title"];
 				$description = $row["description"];
 
+				$title = $this->trimField($title, 55);
+				$description = $this->trimField($description, 230);
+
 				$resultsHtml .= "<div class='resultContainer'><h3 class='title'><a class='result' href='$url'>$title</a></h3><span class='url'>$url</span><span class='description'>$description</span></div>";
 			}
 
 			$resultsHtml .= "</div>";
 
 			return $resultsHtml;
+		}
+
+		private function trimField($string, $characterLimit) {
+			$dots = strlen($string) > $characterLimit ? '...' : '';
+
+			return substr($string, 0, $characterLimit) . $dots;
 		}
 	}
 ?>
--- search.php
+++ search.php
@@ -9,6 +9,7 @@
     }
 
 	$type = isset($_GET['type']) ? $_GET['type'] : 'sites';
+    $page = isset($_GET['page']) ? $_GET['page'] : 1;
 ?>
 
 <!DOCTYPE html>
@@ -34,7 +35,7 @@
 					<div class="searchContainer">
 						<form action="search.php" method="GET">
 							<div class="searchBarContainer">
-								<input type="text" class="searchBox" name="term" title="Enter your search term(s)..." />
+								<input type="text" class="searchBox" name="term" title="Enter your search term(s)..." value="<?php echo $term; ?>" />
 								<button class="searchButton">
                                     <img src="assets/images/icons/search.png" alt="Search" />
                                 </button>
@@ -60,11 +61,47 @@
             <div class="mainResultsSection">
                 <?php
                     $resultsProvider = new SiteResultsProvider($con);
+                    $pageSize = 20;
                     $numResults = $resultsProvider->getNumResults($term);
 
                     echo "<p class='resultsCount'>$numResults results found</p>";
-                    echo $resultsProvider->getResultsHtml(1, 20, $term);
+                    echo $resultsProvider->getResultsHtml($page, $pageSize, $term);
                 ?>
+            </div>
+            <div class="paginationContainer">
+                <div class="pageButtons">
+                    <div class="pageNumberContainer">
+                        <img src="assets/images/pageStart.png" alt="D" />
+                    </div>
+                    <?php
+                        $pagesToShow = 10;
+                        $numPages = ceil($numResults / $pageSize);
+                        $pagesLeft = min($pagesToShow, $numPages);
+                        $currentPage = $page - floor($pagesToShow / 2);
+
+                        if ($currentPage < 1) {
+                            $currentPage = 1;
+                        }
+
+                        if ($currentPage + $pagesLeft > $numPages + 1) {
+                            $currentPage = $numPages + 1 - $pagesLeft;
+                        }
+
+                        while ($pagesLeft != 0 && $currentPage <= $numPages) {
+                            if ($currentPage == $page) {
+	                            echo "<div class='pageNumberContainer'><img src='assets/images/pageSelected.png' alt='o' /><span class='pageNumber'>$currentPage</span></div>";
+                            } else {
+	                            echo "<div class='pageNumberContainer'><a href='search.php?term=$term&type=$type&page=$currentPage'><img src='assets/images/page.png' alt='o' /><span class='pageNumber'>$currentPage</span></a></div>";
+                            }
+
+                            $currentPage++;
+                            $pagesLeft--;
+                        }
+                    ?>
+                    <div class="pageNumberContainer">
+                        <img src="assets/images/pageEnd.png" alt="dle" />
+                    </div>
+                </div>
             </div>
 		</div>
 	</body>
